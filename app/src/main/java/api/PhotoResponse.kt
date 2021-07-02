4e3cb4f87d26f779b9aed37868eeba7f273f1e40
Enerdgazer <35137415+Enerdgazer@users.noreply.github.com>
2021-07-02 16:06:44 +0300

    Initial commit

---
+++ app/src/main/java/api/PhotoResponse.kt
@@ -0,0 +1,9 @@
+package api
+
+import com.example.appphotoflickr.GalleryItem
+import com.google.gson.annotations.SerializedName
+
+class PhotoResponse {
+    @SerializedName("photo")
+    lateinit var galleryItems: List<GalleryItem>
+}
